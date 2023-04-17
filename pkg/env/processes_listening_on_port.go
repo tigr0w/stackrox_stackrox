321519730d1f885a1366c26cd1115a855cbca346
Dave Shrewsberry <dashrews@redhat.com>
2023-04-17 05:51:56 -0400

    revert turning off plop since logging was fixed

--- pkg/env/processes_listening_on_port.go
+++ pkg/env/processes_listening_on_port.go
@@ -2,5 +2,5 @@ package env
 
 var (
 	// ProcessesListeningOnPort enables the NetworkFlow code to also update the processes that are listening on ports
-	ProcessesListeningOnPort = RegisterBooleanSetting("ROX_PROCESSES_LISTENING_ON_PORT", false)
+	ProcessesListeningOnPort = RegisterBooleanSetting("ROX_PROCESSES_LISTENING_ON_PORT", true)
 )
