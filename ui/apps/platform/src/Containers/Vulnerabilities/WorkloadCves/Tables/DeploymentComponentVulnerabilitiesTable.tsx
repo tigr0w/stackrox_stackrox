mdafsanhossain/stackrox

Duplicate component vuln tables for images/deployments

ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Tables/DeploymentComponentVulnerabilitiesTable.tsx
@@ -1,0 +1,126 @@
+import React from 'react';
+import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import { gql } from '@apollo/client';
+
+import useTableSort from 'hooks/patternfly/useTableSort';
+import ImageNameTd from '../components/ImageNameTd';
+import {
+    imageMetadataContextFragment,
+    ImageMetadataContext,
+    DeploymentComponentVulnerability,
+    sortTableData,
+    flattenDeploymentComponentVulns,
+} from './componentVulnerabilities.utils';
+import FixedByVersionTd from '../components/FixedByVersionTd';
+import DockerfileLayerTd from '../components/DockerfileLayerTd';
+
+export { imageMetadataContextFragment };
+export type { ImageMetadataContext, DeploymentComponentVulnerability };
+
+export const deploymentComponentVulnerabilitiesFragment = gql`
+    fragment DeploymentComponentVulnerabilities on ImageComponent {
+        name
+        version
+        location
+        layerIndex
+        imageVulnerabilities(query: $query) {
+            id
+            severity
+            cvss
+            scoreVersion
+            fixedByVersion
+        }
+    }
+`;
+
+const sortFields = ['Image', 'Component'];
+const defaultSortOption = { field: 'Image', direction: 'asc' } as const;
+
+export type DeploymentComponentVulnerabilitiesTableProps = {
+    /** The images and associated component vulnerability data to display in the table */
+    images: {
+        imageMetadataContext: ImageMetadataContext;
+        componentVulnerabilities: DeploymentComponentVulnerability[];
+    }[];
+};
+
+function DeploymentComponentVulnerabilitiesTable({
+    images,
+}: DeploymentComponentVulnerabilitiesTableProps) {
+    const { sortOption, getSortParams } = useTableSort({ sortFields, defaultSortOption });
+    const componentVulns = images.flatMap(({ imageMetadataContext, componentVulnerabilities }) =>
+        flattenDeploymentComponentVulns(imageMetadataContext, componentVulnerabilities)
+    );
+    const sortedComponentVulns = sortTableData(componentVulns, sortOption);
+    return (
+        <TableComposable
+            className="pf-u-p-md"
+            style={{
+                border: '1px solid var(--pf-c-table--BorderColor)',
+            }}
+            borders={false}
+        >
+            <Thead>
+                <Tr>
+                    <Th sort={getSortParams('Image')}>Image</Th>
+                    <Th sort={getSortParams('Component')}>Component</Th>
+                    <Th>CVE Severity</Th>
+                    <Th>CVSS</Th>
+                    <Th>Version</Th>
+                    <Th>CVE Fixed in</Th>
+                    <Th>Location</Th>
+                </Tr>
+            </Thead>
+            {sortedComponentVulns.map((componentVuln, index) => {
+                const {
+                    image,
+                    name,
+                    vulnerabilityId,
+                    severity,
+                    version,
+                    cvss,
+                    scoreVersion,
+                    fixedByVersion,
+                    location,
+                    layer,
+                } = componentVuln;
+                // No border on the last row
+                const style =
+                    index !== componentVulns.length - 1
+                        ? { borderBottom: '1px solid var(--pf-c-table--BorderColor)' }
+                        : {};
+
+                return (
+                    <Tbody key={`${image.id}:${name}:${version}:${vulnerabilityId}`} style={style}>
+                        <Tr>
+                            <Td>
+                                {image.name ? (
+                                    <ImageNameTd name={image.name} id={image.id} />
+                                ) : (
+                                    'Image name not available'
+                                )}
+                            </Td>
+                            <Td>{name}</Td>
+                            <Td>{severity}</Td>
+                            <Td>
+                                {cvss.toFixed(1)} ({scoreVersion})
+                            </Td>
+                            <Td>{version}</Td>
+                            <Td>
+                                <FixedByVersionTd fixedByVersion={fixedByVersion} />
+                            </Td>
+                            <Td>{location || 'N/A'}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td colSpan={6} className="pf-u-pt-0">
+                                <DockerfileLayerTd layer={layer} />
+                            </Td>
+                        </Tr>
+                    </Tbody>
+                );
+            })}
+        </TableComposable>
+    );
+}
+
+export default DeploymentComponentVulnerabilitiesTable;

ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Tables/ImageComponentVulnerabilitiesTable.tsx
@@ -1,0 +1,99 @@
+import React from 'react';
+import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import { gql } from '@apollo/client';
+
+import useTableSort from 'hooks/patternfly/useTableSort';
+import {
+    ImageComponentVulnerability,
+    ImageMetadataContext,
+    flattenImageComponentVulns,
+    imageMetadataContextFragment,
+    sortTableData,
+} from './componentVulnerabilities.utils';
+import FixedByVersionTd from '../components/FixedByVersionTd';
+import DockerfileLayerTd from '../components/DockerfileLayerTd';
+
+export { imageMetadataContextFragment };
+export type { ImageMetadataContext, ImageComponentVulnerability };
+
+export const imageComponentVulnerabilitiesFragment = gql`
+    fragment ImageComponentVulnerabilities on ImageComponent {
+        name
+        version
+        location
+        layerIndex
+        imageVulnerabilities(query: $query) {
+            id
+            severity
+            fixedByVersion
+        }
+    }
+`;
+
+const sortFields = ['Component'];
+const defaultSortOption = { field: 'Component', direction: 'asc' } as const;
+
+export type ImageComponentVulnerabilitiesTableProps = {
+    /** The image and associated component vulnerability data to display in the table */
+    imageMetadataContext: ImageMetadataContext;
+    componentVulnerabilities: ImageComponentVulnerability[];
+};
+
+function ImageComponentVulnerabilitiesTable({
+    imageMetadataContext,
+    componentVulnerabilities,
+}: ImageComponentVulnerabilitiesTableProps) {
+    const { sortOption, getSortParams } = useTableSort({ sortFields, defaultSortOption });
+    const componentVulns = flattenImageComponentVulns(
+        imageMetadataContext,
+        componentVulnerabilities
+    );
+    const sortedComponentVulns = sortTableData(componentVulns, sortOption);
+    return (
+        <TableComposable
+            className="pf-u-p-md"
+            style={{
+                border: '1px solid var(--pf-c-table--BorderColor)',
+            }}
+            borders={false}
+        >
+            <Thead>
+                <Tr>
+                    <Th sort={getSortParams('Component')}>Component</Th>
+                    <Th>Version</Th>
+                    <Th>CVE Fixed in</Th>
+                    <Th>Location</Th>
+                </Tr>
+            </Thead>
+            {sortedComponentVulns.map((componentVuln, index) => {
+                const { image, name, vulnerabilityId, version, fixedByVersion, location, layer } =
+                    componentVuln;
+                // No border on the last row
+                const style =
+                    index !== componentVulns.length - 1
+                        ? { borderBottom: '1px solid var(--pf-c-table--BorderColor)' }
+                        : {};
+
+                return (
+                    <Tbody key={`${image.id}:${name}:${version}:${vulnerabilityId}`} style={style}>
+                        <Tr>
+                            <Td>{name}</Td>
+                            <Td>{version}</Td>
+                            <Td>
+                                <FixedByVersionTd fixedByVersion={fixedByVersion} />
+                            </Td>
+                            <Td>{location || 'N/A'}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td colSpan={4} className="pf-u-pt-0">
+                                <DockerfileLayerTd layer={layer} />
+                            </Td>
+                        </Tr>
+                    </Tbody>
+                );
+            })}
+        </TableComposable>
+    );
+}
+
+export default ImageComponentVulnerabilitiesTable;

ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/DockerfileLayerTd.tsx
@@ -1,0 +1,32 @@
+import React from 'react';
+import { CodeBlock, Flex, CodeBlockCode } from '@patternfly/react-core';
+import { TableDataRow } from '../Tables/componentVulnerabilities.utils';
+
+export type DockerfileLayerTdProps = {
+    layer: TableDataRow['layer'];
+};
+
+function DockerfileLayerTd({ layer }: DockerfileLayerTdProps) {
+    return layer ? (
+        <CodeBlock>
+            <Flex>
+                <CodeBlockCode
+                    // 120px is a width that looks good with the largest dockerfile instruction: "HEALTHCHECK"
+                    style={{ flexBasis: '120px' }}
+                    className="pf-u-flex-shrink-0"
+                >
+                    {layer.line} {layer.instruction}
+                </CodeBlockCode>
+                <CodeBlockCode className="pf-u-flex-grow-1 pf-u-flex-basis-0">
+                    {layer.instruction} {layer.value}
+                </CodeBlockCode>
+            </Flex>
+        </CodeBlock>
+    ) : (
+        <CodeBlock>
+            <CodeBlockCode>Dockerfile layer information not available</CodeBlockCode>
+        </CodeBlock>
+    );
+}
+
+export default DockerfileLayerTd;

tigr0w/stackrox_stackrox

ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Tables/DeploymentComponentVulnerabilitiesTable.tsx
@@ -1,0 +1,126 @@
+import React from 'react';
+import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import { gql } from '@apollo/client';
+
+import useTableSort from 'hooks/patternfly/useTableSort';
+import ImageNameTd from '../components/ImageNameTd';
+import {
+    imageMetadataContextFragment,
+    ImageMetadataContext,
+    DeploymentComponentVulnerability,
+    sortTableData,
+    flattenDeploymentComponentVulns,
+} from './table.utils';
+import FixedByVersionTd from '../components/FixedByVersionTd';
+import DockerfileLayerTd from '../components/DockerfileLayerTd';
+
+export { imageMetadataContextFragment };
+export type { ImageMetadataContext, DeploymentComponentVulnerability };
+
+export const deploymentComponentVulnerabilitiesFragment = gql`
+    fragment DeploymentComponentVulnerabilities on ImageComponent {
+        name
+        version
+        location
+        layerIndex
+        imageVulnerabilities(query: $query) {
+            id
+            severity
+            cvss
+            scoreVersion
+            fixedByVersion
+        }
+    }
+`;
+
+const sortFields = ['Image', 'Component'];
+const defaultSortOption = { field: 'Image', direction: 'asc' } as const;
+
+export type DeploymentComponentVulnerabilitiesTableProps = {
+    /** The images and associated component vulnerability data to display in the table */
+    images: {
+        imageMetadataContext: ImageMetadataContext;
+        componentVulnerabilities: DeploymentComponentVulnerability[];
+    }[];
+};
+
+function DeploymentComponentVulnerabilitiesTable({
+    images,
+}: DeploymentComponentVulnerabilitiesTableProps) {
+    const { sortOption, getSortParams } = useTableSort({ sortFields, defaultSortOption });
+    const componentVulns = images.flatMap(({ imageMetadataContext, componentVulnerabilities }) =>
+        flattenDeploymentComponentVulns(imageMetadataContext, componentVulnerabilities)
+    );
+    const sortedComponentVulns = sortTableData(componentVulns, sortOption);
+    return (
+        <TableComposable
+            className="pf-u-p-md"
+            style={{
+                border: '1px solid var(--pf-c-table--BorderColor)',
+            }}
+            borders={false}
+        >
+            <Thead>
+                <Tr>
+                    <Th sort={getSortParams('Image')}>Image</Th>
+                    <Th sort={getSortParams('Component')}>Component</Th>
+                    <Th>CVE Severity</Th>
+                    <Th>CVSS</Th>
+                    <Th>Version</Th>
+                    <Th>CVE Fixed in</Th>
+                    <Th>Location</Th>
+                </Tr>
+            </Thead>
+            {sortedComponentVulns.map((componentVuln, index) => {
+                const {
+                    image,
+                    name,
+                    vulnerabilityId,
+                    severity,
+                    version,
+                    cvss,
+                    scoreVersion,
+                    fixedByVersion,
+                    location,
+                    layer,
+                } = componentVuln;
+                // No border on the last row
+                const style =
+                    index !== componentVulns.length - 1
+                        ? { borderBottom: '1px solid var(--pf-c-table--BorderColor)' }
+                        : {};
+
+                return (
+                    <Tbody key={`${image.id}:${name}:${version}:${vulnerabilityId}`} style={style}>
+                        <Tr>
+                            <Td>
+                                {image.name ? (
+                                    <ImageNameTd name={image.name} id={image.id} />
+                                ) : (
+                                    'Image name not available'
+                                )}
+                            </Td>
+                            <Td>{name}</Td>
+                            <Td>{severity}</Td>
+                            <Td>
+                                {cvss.toFixed(1)} ({scoreVersion})
+                            </Td>
+                            <Td>{version}</Td>
+                            <Td>
+                                <FixedByVersionTd fixedByVersion={fixedByVersion} />
+                            </Td>
+                            <Td>{location || 'N/A'}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td colSpan={6} className="pf-u-pt-0">
+                                <DockerfileLayerTd layer={layer} />
+                            </Td>
+                        </Tr>
+                    </Tbody>
+                );
+            })}
+        </TableComposable>
+    );
+}
+
+export default DeploymentComponentVulnerabilitiesTable;

ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Tables/ImageComponentVulnerabilitiesTable.tsx
@@ -1,0 +1,99 @@
+import React from 'react';
+import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import { gql } from '@apollo/client';
+
+import useTableSort from 'hooks/patternfly/useTableSort';
+import {
+    ImageComponentVulnerability,
+    ImageMetadataContext,
+    flattenImageComponentVulns,
+    imageMetadataContextFragment,
+    sortTableData,
+} from './table.utils';
+import FixedByVersionTd from '../components/FixedByVersionTd';
+import DockerfileLayerTd from '../components/DockerfileLayerTd';
+
+export { imageMetadataContextFragment };
+export type { ImageMetadataContext, ImageComponentVulnerability };
+
+export const imageComponentVulnerabilitiesFragment = gql`
+    fragment ImageComponentVulnerabilities on ImageComponent {
+        name
+        version
+        location
+        layerIndex
+        imageVulnerabilities(query: $query) {
+            id
+            severity
+            fixedByVersion
+        }
+    }
+`;
+
+const sortFields = ['Component'];
+const defaultSortOption = { field: 'Component', direction: 'asc' } as const;
+
+export type ImageComponentVulnerabilitiesTableProps = {
+    /** The image and associated component vulnerability data to display in the table */
+    imageMetadataContext: ImageMetadataContext;
+    componentVulnerabilities: ImageComponentVulnerability[];
+};
+
+function ImageComponentVulnerabilitiesTable({
+    imageMetadataContext,
+    componentVulnerabilities,
+}: ImageComponentVulnerabilitiesTableProps) {
+    const { sortOption, getSortParams } = useTableSort({ sortFields, defaultSortOption });
+    const componentVulns = flattenImageComponentVulns(
+        imageMetadataContext,
+        componentVulnerabilities
+    );
+    const sortedComponentVulns = sortTableData(componentVulns, sortOption);
+    return (
+        <TableComposable
+            className="pf-u-p-md"
+            style={{
+                border: '1px solid var(--pf-c-table--BorderColor)',
+            }}
+            borders={false}
+        >
+            <Thead>
+                <Tr>
+                    <Th sort={getSortParams('Component')}>Component</Th>
+                    <Th>Version</Th>
+                    <Th>CVE Fixed in</Th>
+                    <Th>Location</Th>
+                </Tr>
+            </Thead>
+            {sortedComponentVulns.map((componentVuln, index) => {
+                const { image, name, vulnerabilityId, version, fixedByVersion, location, layer } =
+                    componentVuln;
+                // No border on the last row
+                const style =
+                    index !== componentVulns.length - 1
+                        ? { borderBottom: '1px solid var(--pf-c-table--BorderColor)' }
+                        : {};
+
+                return (
+                    <Tbody key={`${image.id}:${name}:${version}:${vulnerabilityId}`} style={style}>
+                        <Tr>
+                            <Td>{name}</Td>
+                            <Td>{version}</Td>
+                            <Td>
+                                <FixedByVersionTd fixedByVersion={fixedByVersion} />
+                            </Td>
+                            <Td>{location || 'N/A'}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td colSpan={4} className="pf-u-pt-0">
+                                <DockerfileLayerTd layer={layer} />
+                            </Td>
+                        </Tr>
+                    </Tbody>
+                );
+            })}
+        </TableComposable>
+    );
+}
+
+export default ImageComponentVulnerabilitiesTable;

ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/DockerfileLayerTd.tsx
@@ -1,0 +1,32 @@
+import React from 'react';
+import { CodeBlock, Flex, CodeBlockCode } from '@patternfly/react-core';
+import { TableDataRow } from '../Tables/table.utils';
+
+export type DockerfileLayerTdProps = {
+    layer: TableDataRow['layer'];
+};
+
+function DockerfileLayerTd({ layer }: DockerfileLayerTdProps) {
+    return layer ? (
+        <CodeBlock>
+            <Flex>
+                <CodeBlockCode
+                    // 120px is a width that looks good with the largest dockerfile instruction: "HEALTHCHECK"
+                    style={{ flexBasis: '120px' }}
+                    className="pf-u-flex-shrink-0"
+                >
+                    {layer.line} {layer.instruction}
+                </CodeBlockCode>
+                <CodeBlockCode className="pf-u-flex-grow-1 pf-u-flex-basis-0">
+                    {layer.instruction} {layer.value}
+                </CodeBlockCode>
+            </Flex>
+        </CodeBlock>
+    ) : (
+        <CodeBlock>
+            <CodeBlockCode>Dockerfile layer information not available</CodeBlockCode>
+        </CodeBlock>
+    );
+}
+
+export default DockerfileLayerTd;